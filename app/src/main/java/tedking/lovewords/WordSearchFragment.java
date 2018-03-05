0f91494cb58d42a93fb416adbf9a70720d8e993f
praye <545746909@qq.com>
2018-03-05 21:16:41 +0800

    set clicklistener to button, but method is not implemented yet

--- app/src/main/java/tedking/lovewords/WordSearchFragment.java
+++ app/src/main/java/tedking/lovewords/WordSearchFragment.java
@@ -7,15 +7,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2018/3/4.
  */
 
 public class WordSearchFragment extends android.support.v4.app.Fragment {
+    private Button search;
+    private EditText editText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.word_search_layout, container, false);
+        search = view.findViewById(R.id.btn_search);
+        editText = view.findViewById(R.id.edit_query);
         return view;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        //method to be implemented
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),editText.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
