52cd295e2b68d94cae7d7f351601e0aa388e784b
praye <545746909@qq.com>
2018-03-09 16:46:09 +0800

    add adapter to listview to test function

--- app/src/main/java/tedking/lovewords/AlaramFragment.java
+++ app/src/main/java/tedking/lovewords/AlaramFragment.java
@@ -1,19 +1,48 @@
 package tedking.lovewords;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2018/3/5.
  */
 
 public class AlaramFragment extends Fragment {
+    private ListView alarmList;
+    private SimpleAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.alarm_fragment_layout, container,false);
+        alarmList = view.findViewById(R.id.alarm_list);
+        List<Map<String, Object>> data = new ArrayList<>();
+        Map<String,Object> temp = new LinkedHashMap<>();
+        temp.put("time","8:30");
+        temp.put("repeate_day","工作日");
+        data.add(temp);
+        Map<String,Object> temp1 = new LinkedHashMap<>();
+        temp1.put("time","8:30");
+        temp1.put("repeate_day","工作日");
+        data.add(temp1);
+        adapter = new SimpleAdapter(getContext(),data,R.layout.alarm_item_layout, new String[]{"time","repeate_day"}, new int[]{R.id.time, R.id.repeat_day});
+        alarmList.setAdapter(adapter);
         return view;
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        FloatingActionButton floatingActionButton = getActivity().findViewById(R.id.fab);
+        floatingActionButton.setImageResource(R.drawable.ic_add);
+
     }
 }
