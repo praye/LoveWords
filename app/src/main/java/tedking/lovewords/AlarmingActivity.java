8576bd7b851b275af14df33c5cfcc03c034119d7
praye <545746909@qq.com>
2018-03-16 17:18:45 +0800

    create alarming activity, but not implemented yet

---
+++ app/src/main/java/tedking/lovewords/AlarmingActivity.java
@@ -0,0 +1,13 @@
+package tedking.lovewords;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarming);
+    }
+}
