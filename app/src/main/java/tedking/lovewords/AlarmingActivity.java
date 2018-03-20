63d9a8a1cca1be89f3d6df9df7026cb0add2984d
praye <545746909@qq.com>
2018-03-21 02:00:13 +0800

    add action to the first button, to Exercise Activity

--- app/src/main/java/tedking/lovewords/AlarmingActivity.java
+++ app/src/main/java/tedking/lovewords/AlarmingActivity.java
@@ -15,7 +15,7 @@ public class AlarmingActivity extends Activity {
     private static final long EXECUTE_TIME = 180000;
     private Vibrator vibrator;
     private Handler handler = new Handler();
-    private Button exit;
+    private Button exit, todo;
     long [] pattern = {1000,1000};
 
     @Override
@@ -23,11 +23,12 @@ public class AlarmingActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarming);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON|WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-        Intent intent = new Intent();
+        final Intent intent = new Intent();
         intent.setClass(AlarmingActivity.this,AlarmService.class);
         startService(intent);
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         exit = findViewById(R.id.exit);
+        todo = findViewById(R.id.todo);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -43,6 +44,17 @@ public class AlarmingActivity extends Activity {
             public void onClick(View view) {
                 mediaPlayer.stop();
                 vibrator.cancel();
+                finish();
+            }
+        });
+        todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                vibrator.cancel();
+                Intent intent1 = new Intent();
+                intent1.setClass(AlarmingActivity.this,Exercise.class);
+                startActivity(intent1);
                 finish();
             }
         });
