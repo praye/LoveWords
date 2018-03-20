ae07cc32b891447fef2ea3e0a4e9820b010b8a32
praye <545746909@qq.com>
2018-03-20 15:32:23 +0800

    change to logic of guide next activity

--- app/src/main/java/tedking/lovewords/WelcomeActivity.java
+++ app/src/main/java/tedking/lovewords/WelcomeActivity.java
@@ -1,15 +1,13 @@
 package tedking.lovewords;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.avos.avoscloud.AVUser;
+
 public class WelcomeActivity extends Activity {
-    private SharedPreferences preferences;
-    private SharedPreferences.Editor editor;
     private boolean toGuide;
     private Handler handler = new Handler();
 
@@ -17,15 +15,12 @@ public class WelcomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_welcome);
-        preferences = getSharedPreferences("sharedPreferences", MODE_PRIVATE);
-        toGuide = preferences.getBoolean("guide",true);
+        toGuide = AVUser.getCurrentUser() == null ? true : false;
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 if (toGuide){
                     Intent intent = new Intent(WelcomeActivity.this,GuideActivity.class);
-                    editor = preferences.edit();
-                    editor.putBoolean("guide",false).commit();
                     startActivity(intent);
                     WelcomeActivity.this.finish();
                 }
