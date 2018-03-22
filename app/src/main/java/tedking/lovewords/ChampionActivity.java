4dcfd7f3a48f1a2ef99377d2e819d9f0e5a84557
praye <545746909@qq.com>
2018-03-22 13:45:28 +0800

    done

--- app/src/main/java/tedking/lovewords/ChampionActivity.java
+++ app/src/main/java/tedking/lovewords/ChampionActivity.java
@@ -18,7 +18,7 @@ import java.util.Date;
 
 public class ChampionActivity extends Activity {
     private TextView champion_username, champion_score;
-    private String championOf;
+    private String championOf, className;
 
     //just for test, it will delete anytime;
     private Button testButton;
@@ -42,27 +42,38 @@ public class ChampionActivity extends Activity {
     };
     private void getChampionData(String string){
         Calendar calendar = Calendar.getInstance();
-        Date startDate = new Date(), endDate = new Date();
+        Date startDate = new Date();
         if (string.equals("Today")){
             calendar.set(Calendar.HOUR_OF_DAY,0);
             calendar.set(Calendar.MINUTE,0);
             calendar.set(Calendar.SECOND,0);
             calendar.set(Calendar.MILLISECOND,0);
             startDate = calendar.getTime();
-            calendar.add(Calendar.DAY_OF_MONTH,1);
-            endDate = calendar.getTime();
+            className = "dayScore";
+        }else{
+            calendar.set(Calendar.DAY_OF_WEEK, 1);
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE,0);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.MILLISECOND,0);
+            startDate = calendar.getTime();
+            className = "weekScore";
         }
-        AVQuery<AVObject> query = new AVQuery<>("dayScore");
+        AVQuery<AVObject> query = new AVQuery<>(className);
         query.whereGreaterThanOrEqualTo("createdAt",startDate);
-        query.whereLessThan("createdAt",endDate);
         query.orderByDescending("score");
         query.getFirstInBackground(new GetCallback<AVObject>() {
             @Override
             public void done(AVObject avObject, AVException e) {
                 // object 就是符合条件的第一个 AVObject
                 if (e == null){
-                    champion_username.setText(avObject.getString("user"));
-                    champion_score.setText(avObject.getInt("score") + "");
+                    if (avObject != null){
+                        champion_username.setText(avObject.getString("user"));
+                        champion_score.setText(avObject.getInt("score") + "");
+                    }else {
+                        //TODO
+                        Toast.makeText(ChampionActivity.this,"目前还没有用户上传数据",Toast.LENGTH_SHORT).show();
+                    }
                 }else {
                     e.printStackTrace();
                 }
