5c5cce64af3f8c6ca7f7e27d24ba21a67f31f32f
praye <545746909@qq.com>
2018-03-23 04:18:55 +0800

    create FinishExerciseActivity

---
+++ app/src/main/java/tedking/lovewords/FinishExerciseActivity.java
@@ -0,0 +1,13 @@
+package tedking.lovewords;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FinishExerciseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_finishexercise);
+    }
+}
