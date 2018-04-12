3f4f95c274ff8c9ec24cd1f67ce91d4e46821745
praye <545746909@qq.com>
2018-04-12 13:27:39 +0800

    add notice of data recovery

--- app/src/main/java/tedking/lovewords/RecoveryActivity.java
+++ app/src/main/java/tedking/lovewords/RecoveryActivity.java
@@ -1,6 +1,8 @@
 package tedking.lovewords;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -8,7 +10,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -28,6 +34,9 @@ public class RecoveryActivity extends Activity {
     private SharedPreferences.Editor editor;
     private SQLiteDatabase database;
     private Button button;
+    private Dialog dialog;
+    private int times = 0, sum = 0, expectedSum = 0;
+    private static final  int updateSignal = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +51,14 @@ public class RecoveryActivity extends Activity {
             public void onClick(View view) {
                 if (isNetworkAvailable()){
                     if (!preferences.getBoolean(StringConstant.UPDATEDICTIONARY, false)){
+                        showDialog();
                         setStatus();
                     }else {
-                        Toast.makeText(RecoveryActivity.this,"无需恢复",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(RecoveryActivity.this,"Your data is newest",Toast.LENGTH_SHORT).show();
                     }
 
                 }else {
-                    Toast.makeText(RecoveryActivity.this,"网络出错，请检查你的网络",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RecoveryActivity.this,"Network error!",Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -66,17 +76,29 @@ public class RecoveryActivity extends Activity {
                     query.selectKeys(Arrays.asList("word"));
                     if (i != 0){
                         System.out.println("i =" + i);
-                        int j = i / 1000 + 1;
+                        times = i / 1000 + 1;
+                        for (int a = 0; a < times; a ++){
+                            expectedSum += a;
+                        }
                         final ContentValues cv = new ContentValues();
                         cv.put("status","1");
                         query.limit(1000);
-                        for (int t = 0; t < j; t ++){
+                        for (int t = 0; t < times; t ++){
                             query.skip(t*1000);
+                            final int t1 = t;
                             query.findInBackground(new FindCallback<AVObject>() {
                                 @Override
                                 public void done(List<AVObject> list, AVException e) {
-                                    for (AVObject object : list){
-                                        database.update("words",cv,"english = ?", new String[]{object.getString("word")});
+                                    if(e == null) {
+                                        for (AVObject object : list) {
+                                            database.update("words", cv, "english = ?", new String[]{object.getString("word")});
+                                        }
+                                        Message message = new Message();
+                                        message.what = updateSignal;
+                                        message.obj = t1;
+                                        handler.sendMessage(message);
+                                    }else {
+                                        System.out.println("Network error");
                                     }
                                 }
                             });
@@ -85,9 +107,6 @@ public class RecoveryActivity extends Activity {
                 }
             }
         });
-        editor = preferences.edit();
-        editor.putBoolean(StringConstant.UPDATEDICTIONARY,true);
-        editor.commit();
     }
     @Override
     public void onDestroy(){
@@ -107,5 +126,43 @@ public class RecoveryActivity extends Activity {
                 return false;
             }
         }
+    }
+
+    private Handler handler = new Handler(){
+        public void handleMessage(Message message){
+            int result = (int) message.obj;
+            switch (message.what){
+                case updateSignal:
+                    for (int i = 0; i < times; i ++){
+                        if (result == i){
+                            sum += result;
+                            System.out.println(i);
+                            if (sum == expectedSum){
+                                dialog.dismiss();
+                                Toast.makeText(RecoveryActivity.this,"Data recovered",Toast.LENGTH_LONG).show();
+                                editor = preferences.edit();
+                                editor.putBoolean(StringConstant.UPDATEDICTIONARY,true);
+                                editor.commit();
+                                System.out.println("finish");
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    private void showDialog(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.recovery_loading,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
