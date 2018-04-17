a34f9f66831a1c53499fe18fb4a42b2b823e1d55
praye <545746909@qq.com>
2018-04-17 15:59:29 +0800

    fitSystemStatusBar

--- app/src/main/java/tedking/lovewords/WordDetail.java
+++ app/src/main/java/tedking/lovewords/WordDetail.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -40,6 +41,7 @@ public class WordDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_detail);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         word = getIntent().getStringExtra("word");
         pronunciation = getIntent().getStringExtra("pronunciation");
         findView();
